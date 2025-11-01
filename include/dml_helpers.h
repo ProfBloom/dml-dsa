acda35c15cd057061cdb653173a024f076a50aa9
ProfBloom <lukas.kroenke@web.de>
2025-11-01 12:01:18 +0100

    crc memory move implementation and cmake configuration

--- include/dml_helpers.h
+++ include/dml_helpers.h
@@ -1,9 +1,3 @@
-/*******************************************************************************
- * Copyright (C) 2023 Intel Corporation
- *
- * SPDX-License-Identifier: MIT
- ******************************************************************************/
-
 #ifndef EXAMPLE_UTILS_H
 #define EXAMPLE_UTILS_H
 
@@ -12,34 +6,36 @@
 #include <string.h>
 #include "dml/dml.h"
 
-static inline int parse_execution_path(int argc, char **argv, dml_path_t* path_ptr) {
+namespace dml_helpers  {
+    static inline int parse_execution_path(int argc, char **argv, dml_path_t* path_ptr) {
 
-    if (argc < 2) {
-        printf("Error: Missing the execution path as the first parameter.\n");
-        printf("Use hardware_path, software_path or automatic_path.\n");
-        return 1;
-    }
+        if (argc < 2) {
+            printf("Error: Missing the execution path as the first parameter.\n");
+            printf("Use hardware_path, software_path or automatic_path.\n");
+            return 1;
+        }
 
-    char *execution_path_str = argv[1];
-    if (!strcmp(execution_path_str, "software_path")) {
-        *path_ptr = DML_PATH_SW;
-        printf("The example will be run on the software path.\n");
-    }
-    else if (!strcmp(execution_path_str, "hardware_path")) {
-        *path_ptr = DML_PATH_HW;
-        printf("The example will be run on the hardware path.\n");
-    }
-    else if (!strcmp(execution_path_str, "automatic_path")) {
-        *path_ptr = DML_PATH_AUTO;
-        printf("The example will be run on the automatic path.\n");
-    }
-    else {
-        printf("Error: Unrecognized value for parameter.\n");
-        printf("Use hardware_path, software_path or automatic_path.\n");
-        return 1;
-    }
+        char *execution_path_str = argv[1];
+        if (!strcmp(execution_path_str, "software_path")) {
+            *path_ptr = DML_PATH_SW;
+            printf("The example will be run on the software path.\n");
+        }
+        else if (!strcmp(execution_path_str, "hardware_path")) {
+            *path_ptr = DML_PATH_HW;
+            printf("The example will be run on the hardware path.\n");
+        }
+        else if (!strcmp(execution_path_str, "automatic_path")) {
+            *path_ptr = DML_PATH_AUTO;
+            printf("The example will be run on the automatic path.\n");
+        }
+        else {
+            printf("Error: Unrecognized value for parameter.\n");
+            printf("Use hardware_path, software_path or automatic_path.\n");
+            return 1;
+        }
 
-    return 0;
+        return 0;
+    }
 }
 
 #endif
--- src/memory_move.cpp
+++ src/memory_move.cpp
@@ -1,76 +1,72 @@
+#include <dml/dml.hpp>
 #include <stdio.h>
 #include <stdlib.h>
-#include "dml/dml.h"
-//#include "include/dml_helpers.h"
+#include <iostream>
+#include <numeric>
+#include <vector>
 
-#define BUFFER_SIZE 1024
+constexpr auto size = 1024u;
+
+template <typename path>
+int execute_memory_move()
+{
+    std::cout << "Starting mem move" << std::endl;
+    std::cout << "copy 1KB from source to destination" << std::endl;
+    
+    auto src = std::vector<std::uint8_t>(size);
+    std::iota(src.begin(), src.end(), 0u);
+    auto dst = std::vector<std::uint8_t>(size, 0u);
+
+
+    auto result = dml::execute<path>(dml::mem_move, dml::make_view(src), dml::make_view(dst));
+
+
+    if (result.status == dml::status_code::ok) {
+        std::cout << "Success" << std::endl;
+    }
+    else {
+        std::cout << "Incorrect result" << std::endl;
+        return -1;
+    }
+
+    if (src != dst) {
+        std::cout << "Wrong operation result" << std::endl;
+        return -1;
+    }
+
+    return 0;
+    
+}
 
 int main(int argc, char **argv)
 {
-    dml_path_t execution_path = DML_PATH_SW;
-
-    // int parse_ret = parse_execution_path(argc, argv, &execution_path)
-    // if (parse_ret != 0) {
-    //     return 1;
-    // }
-
-    printf("Start moving region of size 1KB\n");
-
-    dml_job_t *dml_job_ptr = NULL;
-    uint32_t size = 0u;
-
-    uint8_t source[BUFFER_SIZE];
-    uint8_t destination[BUFFER_SIZE];
-
-    for (int i = 0; i < BUFFER_SIZE; i++) {
-        source[i] = i % 256;
-    }
-
-    dml_status_t status = dml_get_job_size(execution_path, &size);
-    if (DML_STATUS_OK != status) {
-        printf("dml_get_job_size failed during getting job size (%u)\n", status);
+    if (argc < 2)
+    {
+        std::cout << "Missing the execution path as the first parameter."
+                  <<  "Use hardware_path, software_path or automatic_path." << std::endl;
         return 1;
     }
 
-    dml_job_ptr = (dml_job_t *)malloc(size);
-
-    status = dml_init_job(execution_path, dml_job_ptr);
-    if (DML_STATUS_OK != status) {
-        printf("dml_init_job failed during job initialization: (%u)\n", status);
-        free(dml_job_ptr);
+    std::string path = argv[1];
+    if (path == "hardware_path")
+    {
+        std::cout << "Executing using dml::hardware path" << std::endl;
+        return execute_memory_move<dml::hardware>();
+    }
+    else if (path == "software_path")
+    {
+        std::cout << "Executing using dml::software path" << std::endl;
+        return execute_memory_move<dml::software>();
+    }
+    else if (path == "auto_path")
+    {
+        std::cout << "Executing using dml::automatic path" << std::endl;
+        return execute_memory_move<dml::automatic>();
+    }
+    else
+    {
+        std::cout << "Unrecognized value for parameter."
+                  << "Use hardware_path, software_path or automatic_path." << std::endl;
         return 1;
     }
-
-    dml_job_ptr->operation = DML_OP_MEM_MOVE;
-    dml_job_ptr->source_first_ptr = source;
-    dml_job_ptr->destination_first_ptr = destination;
-    dml_job_ptr->source_length = BUFFER_SIZE;
-
-    status = dml_execute_job(dml_job_ptr, DML_WAIT_MODE_BUSY_POLL);
-    if (DML_STATUS_OK != status) {
-        printf("An error (%u) occurred during job execution.\n", status);
-        dml_finalize_job(dml_job_ptr);
-        free(dml_job_ptr);
-        return 1;
-    }
-
-    for (int i = 0; i < BUFFER_SIZE; i++) {
-        if (destination[i] != source[i]) {
-            printf("Data mismatch at byte %d: expected %u, got %u\n", i, source[i], destination[i]);
-            dml_finalize_job(dml_job_ptr);
-            free(dml_job_ptr);
-            return 1;
-        }
-    }
-
-    status = dml_finalize_job(dml_job_ptr);
-    if (DML_STATUS_OK != status) {
-        printf("An error (%u) occurred during job finalization.\n", status);
-        free(dml_job_ptr);
-        return 1;
-    }
-    free(dml_job_ptr);
-
-    printf("Job Completed Successfully.\n");
-    return 0;
 }
